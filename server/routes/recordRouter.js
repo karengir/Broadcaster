1a53a5089bd8ef03fe5a963072804948a7cfee76
karen <kgiramata57@gmail.com>
2019-11-27 14:49:24 +0200

    ft(endpoint): Adding the create record endpoint [Finishes 169997365]

---
+++ server/routes/recordRouter.js
@@ -0,0 +1,15 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import Router from 'express';
+import recordController from '../controlers/recordController';
+import tokenVerify from '../helper/tokenVerify';
+import {createRecord} from '../middleware/validation';
+
+
+const router = Router(); 
+
+router.post('/', createRecord, tokenVerify, (req,res) => {
+    recordController.AddRecord(req,res);
+});
+
+
+export default router;
