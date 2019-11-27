2b10778eb105d384bd4286e9f0759fb781a2e7a9
karen <kgiramata57@gmail.com>
2019-11-27 20:52:25 +0200

    ft(endpoint): edited files [Finishes #169999083]

--- server/helper/tokenVerify.js
+++ server/helper/tokenVerify.js
@@ -4,15 +4,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const tokenVerify = (req,res,next)=>{ 
+const tokenVerify = (req,res,next) => { 
     try {
         const token = req.header('token');
         const valid = jwt.verify(token, process.env.SECRET);
         req.user = valid;
-        console.log(token)
+        
+        // console.log(token)
         next();
     } catch (error) {
-       return res.status(400).json({
+        res.status(400).json({
             status: 400,
             message: 'invalid token'
         });
