b00de0dbad00bc4535eb1a6cfe8c9c41c8754bb5
karen <kgiramata57@gmail.com>
2019-12-06 07:39:02 +0200

    ft(database): editing status endpoint [Finishes #170145734]

--- server/v2/helper/tokenVerifyAdmin.js
+++ server/v2/helper/tokenVerifyAdmin.js
@@ -1,30 +1,35 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import users from "../models/users";
+import queries from "../db/queries";
+import executeQuerry from "../db/connection";
 
 dotenv.config();
 
-const tokenVerifyAdmin = (req, res, next) => {
-  try {
-    const token = req.header("token");
-    const valid = jwt.verify(token, process.env.SECRET);
+const tokenVerifyAdmin = async (req, res, next) => {
+  const token = req.header("token");
+  const valid = jwt.verify(token, process.env.SECRET);
+  if (valid) {
     req.user = valid;
     if (req.user.role == "admin") {
-      const checkEmail = users.find(user => user.email == req.user.email);
-      if (!checkEmail) {
+      const checkEmail = await executeQuerry(queries[0].findEmail, [
+        req.user.email
+      ]);
+      if (checkEmail.length === 0) {
         return res.status(401).json({
           status: 401,
-          error: "Not authorized"
+          error: "Not authorized, email does not exist"
         });
       }
       next();
+    } else {
+      return res.status(401).json({
+        status: 401,
+        error: "Not authorized as user"
+      });
     }
-    return res.status(401).json({
-      status: 401,
-      error: "Not authorized"
-    });
-  } catch (error) {
-    res.status(400).json({
+  } else {
+    return res.status(400).json({
       status: 400,
       message: "invalid token"
     });
